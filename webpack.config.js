d0ed5f10e81d2580705b98c9f66d069da7fb54d1
Jonathan Roy <jonaisroy@gmail.com>
2024-06-24 23:14:35 +0300

    fixed loaders

--- webpack.config.js
+++ webpack.config.js
@@ -8,18 +8,22 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
   },
   mode: "development",
-  module: {
-    rules: [
-      {
-        test: /\.(png|jpe?g|gif|svg)$/i,
-        type: "asset/resource",
-      },
-    ],
-  },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, "./src/index.html"),
       filename: "index.html",
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
 };
